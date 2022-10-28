2e3378d54a937a32c1818a99aeffbde6a7e970ed
Doh1024 <dohyeongkim001024@gmail.com>
2022-10-28 17:39:20 -0500

    finished

---
+++ README.md
@@ -0,0 +1,4 @@
+# 262 A2 Dohyeong Kim
+
+## Above and beyond
+I chose custom form styling because the default styling was really ugly and was hard to look at. (line 13-45 in styles.css)
--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,43 @@
-{\rtf1}
+'use strict'
+//global variables
+const button1 = document.querySelector('#color1');
+const button2 = document.querySelector('#color2');
+const button3 = document.querySelector('#color3');
+
+const legend = document.querySelector('legend');
+const heading = document.querySelector('h1');
+const link = document.querySelector('a');
+
+
+const colorChange = () =>{
+
+  //if button # pressed, remove current class and change  heading and legend color to color # and change legend to display "theme #";
+  if(button1.checked){
+    heading.classList.remove('color2', 'color3');
+    legend.classList.remove('color2', 'color3');
+    heading.classList.add('color1');
+    legend.classList.add('color1');
+    legend.innerHTML = "Theme 1";
+  }
+  else if(button2.checked)
+  {
+    heading.classList.remove('color1', 'color3');
+    legend.classList.remove('color1', 'color3');
+    heading.classList.add('color2');
+    legend.classList.add('color2');
+    legend.innerHTML = "Theme 2";
+  }
+  else{
+    heading.classList.remove('color1', 'color2');
+    legend.classList.remove('color1', 'color2');
+    heading.classList.add('color3');
+    legend.classList.add('color3');
+    legend.innerHTML = "Theme 3";
+  }
+
+}
+
+//event listeners
+button1.addEventListener('click', colorChange);
+button2.addEventListener('click', colorChange);
+button3.addEventListener('click', colorChange);
